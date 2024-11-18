f3451da88000b0a8214a5a5ace91de644d3db659
leahthompson01 <leahmthompson03@gmail.com>
2024-11-18 00:31:01 -0700

    style: fixed styling for smaller screen sizes and to be more consistent

--- resources/js/Pages/Todos/Index.tsx
+++ resources/js/Pages/Todos/Index.tsx
@@ -27,11 +27,15 @@ export default function Index({ todosList }: Props) {
         <AuthenticatedLayout>
             <Head title={'todos'} />
             <TodoForm />
-            <div className={'mt-8 flex items-center justify-center gap-4'}>
+            <div
+                className={
+                    'mt-2 flex h-[calc(100vh-350px)] w-[100vw-200px] flex-wrap items-center justify-center gap-4 overflow-y-auto sm:flex-col md:mx-auto md:mt-8 md:h-[calc(100vh-300px)] md:flex-row lg:h-[calc(100vh-300px)] lg:max-w-5xl'
+                }
+            >
                 {todosList.map((el) => (
                     <div
                         className={
-                            'flex h-40 w-80 flex-col gap-4 overflow-y-auto rounded-md bg-gray-700/10 p-4'
+                            'flex h-48 min-w-60 max-w-80 flex-col gap-4 overflow-y-auto rounded-md bg-gray-700/10 p-4'
                         }
                         key={el.id}
                     >
